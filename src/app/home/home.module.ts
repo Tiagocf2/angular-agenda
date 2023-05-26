c7939953bdc233edb56e032140fa9aa1a2b51184
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-05-26 18:36:21 -0300

    feat: post auth page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-import { MatCardModule } from '@angular/material/card';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MatCardModule],
+  declarations: [],
+  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
