62f92a393a58284660df798e2d8a4ae3a80375e3
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-05 21:20:32 -0300

    feat: auth guard

---
+++ src/app/shared/feature/route-guard/public-route.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+import { AppState } from 'src/app/core/data-access/store/reducers';
+
+@Injectable()
+class PublicRouteGuard implements CanActivate {
+  constructor(private store: Store<AppState>, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.store
+      .select((state) => state.auth.isAuth)
+      .pipe(
+        map((isAuth) => {
+          const canActivate = isAuth === false;
+          if (canActivate) return true;
+          this.router.navigate(['/home']);
+          return false;
+        })
+      );
+  }
+}
